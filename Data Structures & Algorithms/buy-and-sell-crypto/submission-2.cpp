602c94f0db0eb18f653a8f5028dbef073142cc10
Parth Khiriya <141862800+ParthKhiriya@users.noreply.github.com>
2026-05-16 14:41:40 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maxProfit = 0;
+
+        int left = 0;
+        int right = 1;
+
+        while(right < n) {
+            if(prices[right] > prices[left]) {
+                maxProfit = max(maxProfit, prices[right] - prices[left]);
+            } else {
+                left = right;
+            }
+            right++;
+        }
+
+        return maxProfit;
+    }
+};
