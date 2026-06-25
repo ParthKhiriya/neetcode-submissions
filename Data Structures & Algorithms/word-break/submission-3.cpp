f5777f44a449236443699319e967994d1aaf5188
Parth Khiriya <141862800+ParthKhiriya@users.noreply.github.com>
2026-06-25 11:55:22 +0530

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    unordered_map<int, bool> memo;
+
+    bool dfs(string& s, vector<string>& wordDict, int i) {
+        if(memo.find(i) != memo.end()) {
+            return memo[i];
+        }
+
+        for(string sub: wordDict) {
+            int len = sub.size();
+            if((s.substr(i, len) == sub)) {
+                if(dfs(s, wordDict, i+len)) {
+                    memo[i] = dfs(s, wordDict, i+len);
+                    return true;
+                }
+            }
+        }
+
+        memo[i] = false;
+        return false;
+    }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        memo[s.length()] = true;
+        return dfs(s, wordDict, 0);
+    }
+};
